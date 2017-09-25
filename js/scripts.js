92cbd91268d6dc58457afb8529a3737f91d426d6
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-24 20:01:19 -0700

    reformat business logic to fully function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,40 @@
 // Business logic goes here
-function isPingPong(userNumber) {
+function pingPongArray(userNumber) {
   var initialNumbers = [];
-  var output;
   for (var i = 1; i <= userNumber; i++) {
     initialNumbers.push(i)
-  };
+  }
+
+  for (var i = 0; i < initialNumbers.length; i++) {
+    var number = initialNumbers[i];
+    if ((number % 5 === 0) && (number % 3 === 0)) {
+      initialNumbers[i] = 'ping-pong';
+    } else if (number % 5 === 0) {
+      initialNumbers[i] = "pong";
+    } else if (number % 3 === 0) {
+      initialNumbers[i] = "ping";
+    }
+  }
+  return initialNumbers;
 }
-initialNumbers.forEach(function(initialNumber) {
-  if 
-}
 
 
-
-
-}
-} return numbers
-
-
-
-
-
+  // initialNumbers.forEach(function(number) {
+  //   if ((number % 5 === 0) && (number % 3 === 0)) {
+  //   }
+  // }
 
 // UI logic goes here
 $(document).ready(function() {
   $("form#pingForm").submit(function(event) {
       event.preventDefault();
       var userNumber = parseInt($("input#number").val());
-      var outcome = isPingPong(userNumber);
+      var outcome = pingPongArray(userNumber);
 
 
-    //   if (userNumber === 0) {
-    //     alert("please use a number greater than zero")
-    //   }
+      if (userNumber === 0) {
+        alert("please use a number greater than zero")
+      }
     //
     //   numbers.forEach(function(number) {
     //   if (three === false && five === false) {
@@ -44,6 +47,9 @@ $(document).ready(function() {
     //     $("#result").append("<li>" + "pong" + "</li>");
     //   }
     // });
+    for  (var i = 0; i < outcome.length; i++) {
+      $("#result").append("<li>" + outcome[i] + "</li>")
+    }
 
       $("#result").show();
   });
